418756d0f53326fe73b6f61201bb265caa581133
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2013-12-07 15:37:51 +0100

    Skeleton implemented

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 lenis0012
 =========
 
-lenis0012
+lenis0012's ten.java repo.
+Livestream url: http://www.twitch.tv/bluedevonmovies
+Starting at: 4PM (UTC)
---
+++ src/main/java/com/lenis0012/tenjava/Core.java
@@ -0,0 +1,16 @@
+package com.lenis0012.tenjava;
+
+/**
+ * Ten Java Contest.
+ * Starting at: 4PM
+ * 
+ * @author lenis0012
+ */
+public class Core {
+	/*
+	 * 
+	 * Oh, its just a skeleton.
+	 * Aha, makes sense.
+	 * 
+	 */
+}
